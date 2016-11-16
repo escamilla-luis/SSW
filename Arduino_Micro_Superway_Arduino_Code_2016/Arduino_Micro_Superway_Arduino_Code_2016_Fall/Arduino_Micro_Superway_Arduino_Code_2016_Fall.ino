36126d6a37958b0f0bd6a9ed85f8a35daeb7e2c1
Luis Escamilla <Luis Escamilla>
2016-11-15 23:53:50 -0800

    made changes to the code. LED status is on there and the state messages that will be sent to the Uno

--- Arduino_Micro_Superway_Arduino_Code_2016/Arduino_Micro_Superway_Arduino_Code_2016_Fall/Arduino_Micro_Superway_Arduino_Code_2016_Fall.ino
+++ Arduino_Micro_Superway_Arduino_Code_2016/Arduino_Micro_Superway_Arduino_Code_2016_Fall/Arduino_Micro_Superway_Arduino_Code_2016_Fall.ino
@@ -5,7 +5,6 @@
 #define RED     6      // STOP and error status
 #define GREEN   5      // GO status 
 #define BLUE    3      // status of podcar
-#define COMMON_ANODE // Type of RGB LED
 #define BUTTON  2      // Kill Switch
 
 #define IS_PRESSED  LOW
@@ -19,17 +18,22 @@ int minimumRange = 0;    // Minimum range needed
 long duration, distance; // Duration used to calculate distance
 int stop_counter = 0;
 int restar_counter = 0;
-String state = "";
 
+String state = "";       // Message used to tell the state of the podcar
+int status;              // Stores status code of podcar
+int reed_switch_state;   // Stores the state of reed_switch reading from Uno
+  
 void setup() 
 {
  Wire.begin(8);     // join the i2c bus with address #8 
   
  pinMode(trigPin, OUTPUT);
  pinMode(echoPin, INPUT);
+ 
  pinMode(RED, OUTPUT);
  pinMode(GREEN, OUTPUT);
  pinMode(BLUE, OUTPUT);
+ 
  pinMode(BUTTON,INPUT_PULLUP);
 
  // change this to 57600 for XBEE comm
@@ -59,9 +63,6 @@ void loop()
  
  Wire.onRequest(requestDistance);       // distance requested by master device
  delay(100);
-
- Wire.onRequest(requestStatus);
- delay(100);
  
  Serial.print(duration);
  Serial.print("\t\t\t");
@@ -71,53 +72,76 @@ void loop()
  // changed distance from 30 cm to 15 cm due to pole obstacle
  if (distance <= 15) 
  {
-  setColor(255, 0, 0);
-  delay(100);
-  setColor(255, 0, 0);
-  delay(100);
-  setColor(255, 0, 0);
+  status_LED(0);
+
+  state = "Collision Avoided";
+
+  Wire.onRequest(requestStatus);
   delay(100);
   
   stop_counter++;
  }
  else if (digitalRead(BUTTON) == IS_PRESSED)
  {
-  setColor(255, 0, 0);
-  
+  status_LED(2);
+   
   state = "Emergency Stop";
+
+  Wire.onRequest(requestStatus);
+  delay(100);
  }
  else
  {
+  status_LED(1);
+
+  state = "Vehicle in motion";
+
+  Wire.onRequest(requestStatus);
+  delay(100);
+  
   stop_counter = 0;
-  setColor(0, 255, 0);
  }
-   //Serial.println(stop_counter);
-   if (stop_counter == 10)
-   {
-    restar_counter = 0;
+   
+ if (stop_counter == 10)
+ {
+  restar_counter = 0;
+  
+  Serial.print("\t\t\t\t");
+  Serial.println();
     
-    state = "Warning: Out of range!";
-    Serial.print("\t\t\t\t");
-    Serial.println();
-    
-    stop_counter = 0;
-    delay(1000);
+  stop_counter = 0;
+  delay(1000);
     
     }
  //Delay 50ms before next reading.
  delay(50);
 }
 
-void setColor(int red, int green, int blue)
+void status_LED(int status)
 {
-  #ifdef COMMON_ANODE
-    red = 255 - red;
-    green = 255 - green;
-    blue = 255 - blue;
-  #endif
-  analogWrite(RED, red);
-  analogWrite(GREEN, green);
-  analogWrite(BLUE, blue);  
+  if (status == 0)
+  {
+    analogWrite(RED, 255);  
+  }
+  else if (status == 1)
+  {
+    analogWrite(GREEN, 255);
+  }
+  else if (status == 2)
+  {
+    for (int i=0 ; i <= 255; i++)
+    {
+      analogWrite(RED, 255);
+      delay(500);
+
+      analogWrite(RED, 0);
+      delay(500);
+    }
+  }
+  else if (status == 3)
+  {
+    analogWrite(BLUE, 255);
+  }
 }
 
 void requestDistance()
@@ -127,6 +151,13 @@ void requestDistance()
 
 void requestStatus()
 {
+  char buffer[32];
   
+  if (status == 0)
+  {
+    state.toCharArray(buffer, 32);
+    
+    Wire.write(buffer);
+  }
 }
 
