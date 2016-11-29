703bff872f907a8685a2813ae3990d0b351b6a6d
Steven Mou <stevenmou94@gmail.com>
2016-11-28 19:27:07 -0800

    Added Collision Test File Steven

---
+++ Test_Code/getStationNumber/Station_Id_Test_Master/Station_Id_Test_Master.ino
@@ -0,0 +1,22 @@
+#include <Wire.h>
+
+void setup(){
+  Wire.begin(8);
+  Serial.begin(9600);
+  
+}
+
+void loop(){
+Wire.requestFrom(8, 1);
+while (Wire.available())
+{
+  int StationId = Wire.read();
+  Serial.print("Station Id: ");
+  Serial.print(StationId);
+  Serial.print("\n");
+  Wire.endTransmission();
+}
+delay(1000);
+}
+
+
