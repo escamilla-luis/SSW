388d86c8db16930f6a8c97f82820f700d33c721b
Johnny Nguyen <johnnytn.mail@gmail.com>
2017-02-15 17:21:55 -0800

    Added log messages for better demonstration.

--- Firebase_JavaScript_Code/client1.js
+++ Firebase_JavaScript_Code/client1.js
@@ -11,10 +11,12 @@ client.on('assignTicket', function(message, data) {
 	
 	var firebaseUserId = data.firebaseUserId;
 	console.log("user id passed from server is: " + firebaseUserId);
+	console.log("Doing relevant work now...");
 	
 	// Reply to server letting it know that task is complete 
 	var podNumber = POD_NUM;
 	var podStatus = -1;
 	
+	console.log("Work done! Replying to server w/ updated status");
 	message.reply({podNumber: podNumber, podStatus: podStatus});
 });
