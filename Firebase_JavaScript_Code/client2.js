1cf9580445f4b23a53aac94243eacf83b27c48c4
Tim Heflin <timmyheflin2@gmail.com>
2017-03-04 21:39:54 -0800

    fixed

---
+++ Firebase_JavaScript_Code/client2.js
@@ -0,0 +1,21 @@
+var messenger = require('messenger');
+var firebase = require('firebase');
+
+var PORT = 8002;
+var POD_NUM = 2;
+
+var client = messenger.createListener(PORT);
+
+client.on('assignTicket', function(message, data) {
+    
+    var firebaseUserId = data.firebaseUserId;
+    console.log("user id passed from server is: " + firebaseUserId);
+    console.log("Doing relevant work now...");
+    
+    // Reply to server letting it know that task is complete 
+    var podNumber = POD_NUM;
+    var podStatus = -1;
+    
+    console.log("Work done! Replying to server w/ updated status");
+    message.reply({podNumber: podNumber, podStatus: podStatus});
+});
