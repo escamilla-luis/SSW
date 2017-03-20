7b799c865e3fd76c85399a405931c784d925de30
Johnny Nguyen <johnnytn.mail@gmail.com>
2017-03-20 00:43:14 -0700

    Updated firebase-interface interface functions

--- Server/interfaces/firebase-interface.js
+++ Server/interfaces/firebase-interface.js
@@ -31,12 +31,38 @@ function getSnapshot(ref, callback) {
         });
 }
 
+// Parses ticket data into a JSON object, used
+// internally by getAllCurrentTicketTicketsAsJsonArray(..)
+function getCurrentTicketJson(userId, onDataReceived) {
+    var ticketRef = getCurrentTicketFromUser(userId);
+    ticketRef.once('value')
+             .then(function(snapshot) {
+                var eta = snapshot.child('eta').val();
+                var from = snapshot.child('from').val();
+                var to = snapshot.child('to').val();
+                var status = snapshot.child('status').val();
+                var isNewTicket = snapshot.child('isNewTicket').val();
+                var timerOn = snapshot.child('timerOn').val();
+                
+                // Return JSON data in callback
+                onDataReceived({
+                    eta: eta,
+                    from: from,
+                    to: to,
+                    status: status,
+                    isNewTicket: isNewTicket,
+                    timerOn: timerOn
+                });
+            });
+}
+
+
 /*** --= QUERY FUNCTIONS =-- ***/
 
 // Parses the ticket into a JSON object that is returned in a callback function
 // -userId: String of the user's id in Firebase
-// -onDataReceived: callback function which we pass our JSON ticket to
-exports.getCurrentTicketJSON = function getCurrentTicketJSON(userId, onDataReceived) {
+// -onDataReceived: callback function which we pass the JSON ticket to
+exports.getCurrentTicketJson = function getCurrentTicketJson(userId, onDataReceived) {
     var ticketRef = getCurrentTicketFromUser(userId);
     ticketRef.once('value')
              .then(function(snapshot) {
@@ -53,7 +79,37 @@ exports.getCurrentTicketJSON = function getCurrentTicketJSON(userId, onDataRecei
                     to: to,
                     status: status,
                     isNewTicket: isNewTicket,
-                    timerOn: timeOn
+                    timerOn: timerOn
+                });
+            });
+}
+
+// Parses tickets into an array of JSON objects
+// -onRetrieve: the callback function which we will pass the JSON array to
+exports.getAllCurrentTicketsAsJsonArray = function getAllCurrentTicketsAsJsonArray(onRetrieve) {
+    var currentTicketsJsonArray = [];
+    usersRef.once('value')
+            .then(function (usersSnapshot) {
+                
+                // Iterate through each user and add their currentTicket Id's
+                var userCount = 0;
+                usersSnapshot.forEach(function(user) {
+                    userCount = userCount + 1;
+                });
+                
+                // Dirty way to ensure each ticket is pushed as we go through multiple
+                // (network) callback functions to add json to the array
+                var count = 0;
+                usersSnapshot.forEach(function(user) {
+                    var userId = user.key;
+                    getCurrentTicketJson(userId, function(ticketData) {
+                        currentTicketsJsonArray.push(ticketData);
+                        count = count + 1;
+                        if (count == userCount) {
+                            onRetrieve(currentTicketsJsonArray);
+                            return;
+                        }
+                    });
                 });
             });
 }
@@ -86,10 +142,27 @@ exports.setEta = function setEta(userId, time) {
     ticketRef.child('eta').set(time);
 }
 
-// Sets the status code for a ticket under a particular user
+// Sets the timerOn value for a user's current ticket data
 // -userId: String of user's id in Firebase
 // -statusCode: Integer that specifies the status code
 exports.setStatus = function setStatus(userId, statusCode) {
     var ticketRef = getCurrentTicketFromUser(userId);
     ticketRef.child('status').set(statusCode);
 }
+
+// Sets the timerOn value for a user's current ticket data
+// -userId: String of user's id in Firebase
+// -timerOn: Boolean that specifies of the timer is on or not
+exports.setTimerOn = function setTimerOn(userId, timerOn) {
+    var ticketRef = getCurrentTicketFromUser(userId);
+    ticketRef.child('timerOn').set(timerOn);
+}
+
+// Sets the isNewTicket value for a user's current ticket data
+// -userId: String of the user's id in Firebase
+// -isNewTicket: Boolean value that specifies if isNewTicket is true or false
+exports.setIsNewTicket = function setIsNewTicket(userId, isNewTicket) {
+    var ticketRef = getCurrentTicketFromUser(userId);
+    ticketRef.child('isNewTicket').set(isNewTicket);
+}
+
